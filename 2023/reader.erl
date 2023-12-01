52104bdc016167f9c545506812ee4bba930bcce0
djs21 <djs21@ic.ac.uk>
2023-12-01 08:17:47 +0000

    added get_ints function

--- 2023/reader.erl
+++ 2023/reader.erl
@@ -1,6 +1,7 @@
 -module(reader).
 -import(lists,[reverse/1]).
--export([readlines/1]).
+-import(string,[to_integer/1]).
+-export([readlines/1, get_ints/1]).
 
 readlines(FileName) ->
     {ok, Device} = file:open(FileName, [read]),
@@ -12,4 +13,11 @@ get_all_lines(Device) ->
     case io:get_line(Device, "") of
         eof  -> [];
         Line -> [Line | get_all_lines(Device)]
+    end.
+
+get_ints([]) -> [];
+get_ints(S) ->
+    case to_integer(S) of
+        {error, _} -> [_|T] = S, get_ints(T);
+        {Int, Rest} -> [Int|get_ints(Rest)]
     end.
